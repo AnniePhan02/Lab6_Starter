8614280cee4ed9503aca5edc984d4a546223de69
AnniePhan02 <a5phan@ucsd.edu>
2023-05-06 20:44:01 -0700

    most of it

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,7 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  return JSON.parse(localStorage.getItem('recipes')) || [];
 }
 
 /**
@@ -35,10 +36,19 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  const refToMain = document.querySelector('main');
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  recipes.forEach(addRecipe);
+
+  function addRecipe(item) {
+    const newRecipeCard = document.createElement('recipe-card');
+    newRecipeCard.data = item;
+    refToMain.appendChild(newRecipeCard);
+  }
+
 }
 
 /**
@@ -51,6 +61,7 @@ function saveRecipesToStorage(recipes) {
   // B1. TODO - Complete the functionality as described in this function
   //            header. It is possible in only a single line, but should
   //            be no more than a few lines.
+  localStorage.setItem('recipes', JSON.stringify(recipes));
 }
 
 /**
@@ -60,7 +71,7 @@ function saveRecipesToStorage(recipes) {
 function initFormHandler() {
 
   // B2. TODO - Get a reference to the <form> element
-  
+  const refForm = document.querySelector('form');
   // B3. TODO - Add an event listener for the 'submit' event, which fires when the
   //            submit button is clicked
 
